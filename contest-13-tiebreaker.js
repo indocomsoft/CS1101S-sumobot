f63d7cd40bb64dd033e186fec0385a028b25529d
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2017-10-20 11:44:27 +0800

    Tiebreaker program: Anyhow whack

--- contest-13-tiebreaker.js
+++ contest-13-tiebreaker.js
@@ -3,7 +3,7 @@ var ev3 = require('./node_modules/ev3source/ev3.js');
 var source = require('./node_modules/ev3source/source.js');
 
 // Needs calibration
-var maxSpeed = 800;
+var maxSpeed = 1000;
 
 // Time required to turn 90 degrees
 var time90deg = 3500;
@@ -14,24 +14,24 @@ var eyeThreshold = 15;
 var leftMotor = ev3.motorB();
 var rightMotor = ev3.motorC();
 
-ev3.runForTime(leftMotor, 4000, maxSpeed);
-ev3.runForTime(rightMotor, 4000, maxSpeed);
-ev3.pause(4000);
-ev3.runForTime(leftMotor, 3300, -maxSpeed);
-ev3.runForTime(rightMotor, 3300, maxSpeed);
-ev3.pause(3300);
+// ev3.runForTime(leftMotor, 4000, maxSpeed);
+// ev3.runForTime(rightMotor, 4000, maxSpeed);
+// ev3.pause(4000);
+// ev3.runForTime(leftMotor, 3300, -maxSpeed);
+// ev3.runForTime(rightMotor, 3300, maxSpeed);
+// ev3.pause(3300);
 
 //ev3.waitForButtonPress();
 while(true) {
-    ev3.runForTime(leftMotor, 6000, maxSpeed);
-    ev3.runForTime(rightMotor, 6000, maxSpeed);
-    ev3.pause(6000);
+    ev3.runForTime(leftMotor, 6500, maxSpeed);
+    ev3.runForTime(rightMotor, 6500, maxSpeed);
+    ev3.pause(6500);
     ev3.runForTime(leftMotor, 3300, -maxSpeed);
     ev3.runForTime(rightMotor, 3300, maxSpeed);
     ev3.pause(3300);
-    ev3.runForTime(leftMotor, 9000, maxSpeed);
-    ev3.runForTime(rightMotor, 9000, maxSpeed);
-    ev3.pause(9000);
+    ev3.runForTime(leftMotor, 9500, maxSpeed);
+    ev3.runForTime(rightMotor, 9500, maxSpeed);
+    ev3.pause(9500);
     ev3.runForTime(leftMotor, 3300, -maxSpeed);
     ev3.runForTime(rightMotor, 3300, maxSpeed);
     ev3.pause(3300);
